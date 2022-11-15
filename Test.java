a79a97264af103e50139a034dc905569c86e7018
Yuri228999 <kerembosa@gmail.com>
2022-11-15 20:08:14 +0300

    Task 1

---
+++ Test.java
@@ -0,0 +1,13 @@
+package ua.com.sourceit.Hello;
+
+import java.util.Scanner;
+
+public class Test {
+	public static void main(String[] args) {
+		 byte code = 65;
+		  System.out.print((char)code);
+		
+	 }
+		
+    
+}
